a9abad30e7142ac2fa67313b05920637d5f0adaa
njood97202 <njood97202@gmail.com>
2022-03-14 15:08:46 +0300

    gg

--- hayat_gp2_18-master/lib/offers/offer_details.dart
+++ hayat_gp2_18-master/lib/offers/offer_details.dart
@@ -142,6 +142,7 @@ class offerDetailes extends StatefulWidget {
 }
 
 class _offerDetailes extends State<offerDetailes> {
+  List<ParseObject> donor = <ParseObject>[];
   var nameOfDonor;
   var SelectedOfferCategory;
   var SelectedOfferStatus;
@@ -150,6 +151,10 @@ class _offerDetailes extends State<offerDetailes> {
   var SelectedPic;
   var SelectedDonorId;
   var SelectedDonor;
+  var name;
+  var phone;
+  var location;
+  var establishmentType;
 
   _offerDetailes(
       this.SelectedOfferCategory,
@@ -167,95 +172,122 @@ class _offerDetailes extends State<offerDetailes> {
   }
 
   void getDonor(String SelectedDonorId) async {
-    QueryBuilder<ParseObject> queryUsers =
-        QueryBuilder<ParseObject>(ParseObject('User'))
-          ..whereEqualTo('objectId', SelectedDonorId);
-    final ParseResponse parseResponse = await queryUsers.query();
-    if (parseResponse.success && parseResponse.results != null) {
-      final object = (parseResponse.results!.first) as ParseObject;
-      //print('name: ${object.get<String>('name')}');
+    QueryBuilder<ParseUser> queryUsers =
+        QueryBuilder<ParseUser>(ParseUser.forQuery())
+          ..whereEqualTo("objectId", SelectedDonorId);
+
+    final ParseResponse apiResponse = await queryUsers.query();
+
+    if (apiResponse.success) {
+      setState(() {
+        donor = apiResponse.results as List<ParseObject>;
+        print('donors');
+        print(donor);
+        print(donor[0].get('name'));
+      });
+      name == donor[0].get("name").toString();
+      phone = donor[0].get("phone").toString();
+      // location = donor[0].get("location").toString();
+      establishmentType = donor[0].get("type").toString();
     } else {
-      print('donor from japan');
-      print(SelectedDonor);
+      donor = [];
     }
-    //  nameOfDonor = SelectedDonor.get("name").toString();
   }
 
-  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
-  }
-}
+    var SelectedOfferCategory;
+    var SelectedOfferStatus;
+    var SelectedAvailableQuantity;
+    var SelectedExpirationDate;
+    var SelectedPic;
+    var SelectedDonorId;
+    var SelectedDonor;
 
-Widget build(BuildContext context) {
-  var SelectedOfferCategory;
-  var SelectedOfferStatus;
-  var SelectedAvailableQuantity;
-  var SelectedExpirationDate;
-  var SelectedPic;
-  var SelectedDonorId;
-  var SelectedDonor;
-
-  return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.blueGrey[200],
-      appBar: AppBar(
-        title: Text(
-          'Hayat food donation',
+    var C = this.SelectedOfferCategory;
+    var S = this.SelectedOfferStatus;
+    var A = this.SelectedAvailableQuantity;
+    var E = this.SelectedExpirationDate;
+    var P = this.SelectedPic;
+    var I = this.SelectedDonorId;
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.blueGrey[200],
+        appBar: AppBar(
+          title: Text(
+            'Hayat food donation',
+          ),
+          backgroundColor: Colors.teal[200],
+          elevation: 0.0,
         ),
-        backgroundColor: Colors.teal[200],
-        elevation: 0.0,
-      ),
-      body: Container(
-        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        padding: EdgeInsets.all(10),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(
-              Radius.circular(10.0) //                 <--- border radius here
+        body: Container(
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          padding: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(
+                Radius.circular(10.0) //                 <--- border radius here
+                ),
+            color: Colors.white70,
+          ),
+          child: ListView(
+            scrollDirection: Axis.vertical,
+            children: <Widget>[
+              Text(
+                'Donation offer details',
+                style: TextStyle(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
               ),
-          color: Colors.white70,
-        ),
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: <Widget>[
-            Text(
-              'Donation offer details',
-              style: TextStyle(fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            Text('\n\n\nFood Category of the Donation: ',
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            Text(
-              '\n' + SelectedOfferCategory,
-              overflow: TextOverflow.visible,
-            ),
-            Text('\nFood Status of Donation: ',
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            Text(
-              '\n' + SelectedOfferStatus,
-              overflow: TextOverflow.visible,
-            ),
-            Text('\nAvailable Quantity (# person): ',
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            Text(
-              '\n' + SelectedAvailableQuantity,
-              overflow: TextOverflow.visible,
-            ),
-            Text('\nExpiration date: ',
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            Text(
-              '\n' + SelectedExpirationDate + '\n',
-              overflow: TextOverflow.visible,
-            ),
-            Divider(color: Colors.grey),
-            Text(
-              '\nDonor information',
-              style: TextStyle(fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-      ));
+              Text('\n\n\nFood Category of the Donation: ',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(
+                '\n' + C,
+                overflow: TextOverflow.visible,
+              ),
+              Text('\nFood Status of Donation: ',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(
+                '\n' + S,
+                overflow: TextOverflow.visible,
+              ),
+              Text('\nAvailable Quantity (# person): ',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(
+                '\n' + A,
+                overflow: TextOverflow.visible,
+              ),
+              Text('\nExpiration date: ',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(
+                '\n' + E + '\n',
+                overflow: TextOverflow.visible,
+              ),
+              Divider(color: Colors.grey),
+              Text(
+                '\nDonor information',
+                style: TextStyle(fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              /*  Text('\n\n\nDonor type: ',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(
+                '\n' + establishmentType,
+                overflow: TextOverflow.visible,
+              ),
+              Text('\nName: ', style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(
+                '\n' + name,
+                overflow: TextOverflow.visible,
+              ),
+              Text('\nContact number: ',
+                  style: TextStyle(fontWeight: FontWeight.bold)),
+              Text(
+                '\n' + phone,
+                overflow: TextOverflow.visible,
+              ),
+              Text('\nlocation: ',
+                  style: TextStyle(fontWeight: FontWeight.bold)),*/
+            ],
+          ),
+        ));
+  }
 }
