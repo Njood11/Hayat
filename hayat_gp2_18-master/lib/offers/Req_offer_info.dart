3a3665e56d673689e55da7bd29cfea2528f11e3e
njood97202 <njood97202@gmail.com>
2022-03-15 14:39:12 +0300

    aa

--- hayat_gp2_18-master/lib/offers/Req_offer_info.dart
+++ hayat_gp2_18-master/lib/offers/Req_offer_info.dart
@@ -103,7 +103,6 @@ class _ReqOfferInfo extends State<ReqOfferInfo> {
     var SelectedPic;
     var SelectedDonorId;
     var SelectedDonor;
-
     var C = this.SelectedOfferCategory;
     var S = this.SelectedOfferStatus;
     var A = this.SelectedAvailableQuantity;
@@ -115,75 +114,148 @@ class _ReqOfferInfo extends State<ReqOfferInfo> {
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.blueGrey[200],
       body: Container(
-        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-        padding: EdgeInsets.all(10),
-        alignment: Alignment.center,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(
-              Radius.circular(10.0) //                 <--- border radius here
+          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+          padding: EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(
+                Radius.circular(10.0) //                 <--- border radius here
+                ),
+            color: Colors.white70,
+          ),
+          child: ListView(scrollDirection: Axis.vertical, children: <Widget>[
+            Container(
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        '\nDonation offer details',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      child: ListTile(
+                        title: Text(
+                          '\nFood category of the donation: \n',
+                          overflow: TextOverflow.visible,
+                        ),
+                        subtitle: Text(C),
+                      ),
+                    ),
+                    Container(
+                      child: ListTile(
+                        title: Text(
+                          '\nFood Status of Donation: \n',
+                          overflow: TextOverflow.visible,
+                        ),
+                        subtitle: Text(S),
+                      ),
+                    ),
+                    Container(
+                      child: ListTile(
+                        title: Text(
+                          '\nAvailable quantity (# person): \n',
+                          overflow: TextOverflow.visible,
+                        ),
+                        subtitle: Text(A),
+                      ),
+                    ),
+                    Container(
+                      child: ListTile(
+                        title: Text(
+                          '\nExpiration date: : \n',
+                          overflow: TextOverflow.visible,
+                        ),
+                        subtitle: Text(E),
+                      ),
+                    ),
+                    Divider(color: Colors.grey),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16),
+                      child: ElevatedButton(
+                          onPressed: () async {
+                            // set up the buttons
+                            Widget cancelButton = TextButton(
+                              child: Text("Ok"),
+                              onPressed: () {
+                                Navigator.pop(context, 'OK');
+                              },
+                            );
+                            Widget continueButton = TextButton(
+                              child: Text("Contact"),
+                              onPressed: () {
+                                //what's app
+                              },
+                            );
+
+                            // set up the AlertDialog
+                            AlertDialog alert = AlertDialog(
+                              title: Text(
+                                '\nDonor Information',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,
+                              ),
+                              content: Container(
+                                child: Text(
+                                  '\nDonor type:  ' +
+                                      donor[0].get("type").toString() +
+                                      '\nName:  ' +
+                                      donor[0].get("name").toString() +
+                                      '\nContact number:  ' +
+                                      donor[0].get("phone").toString() +
+                                      '\nLocation:  ' +
+                                      donor[0].get("location").toString(),
+                                  overflow: TextOverflow.visible,
+                                ),
+                              ),
+                              actions: [
+                                cancelButton,
+                                continueButton,
+                              ],
+                            );
+
+                            // show the dialog
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return alert;
+                              },
+                            );
+                          },
+                          child: const Text('Donor Information')),
+                    ),
+                    Container(
+                      child: ListTile(
+                        title: Text(
+                          '\nDonor Information',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                    /*\Container(
+                      child: ListTile(
+                        title: Text(
+                          '\nDonor type:  ' +
+                              donor[0].get("type").toString() +
+                              '\nName:  ' +
+                              donor[0].get("name").toString() +
+                              '\nContact number:  ' +
+                              donor[0].get("phone").toString() +
+                              '\nLocation:  ' +
+                              donor[0].get("location").toString(),
+                          overflow: TextOverflow.visible,
+                        ),
+                      ),
+                    ),*/
+                  ],
+                ),
               ),
-          color: Colors.white70,
-        ),
-        child: ListView(
-          scrollDirection: Axis.vertical,
-          children: <Widget>[
-            Text(
-              '\nDonation offer details',
-              style: TextStyle(fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
             ),
-            ListTile(
-              title: Text(
-                '\nFood category of the donation: \n',
-                overflow: TextOverflow.visible,
-              ),
-              subtitle: Text(C),
-            ),
-            ListTile(
-              title: Text(
-                '\nFood Status of Donation: \n',
-                overflow: TextOverflow.visible,
-              ),
-              subtitle: Text(S),
-            ),
-            ListTile(
-              title: Text(
-                '\nAvailable quantity (# person): \n',
-                overflow: TextOverflow.visible,
-              ),
-              subtitle: Text(A),
-            ),
-            ListTile(
-              title: Text(
-                '\nExpiration date: : \n',
-                overflow: TextOverflow.visible,
-              ),
-              subtitle: Text(E),
-            ),
-            Divider(color: Colors.grey),
-            ListTile(
-              title: Text(
-                '\nDonor Information',
-                style: TextStyle(fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            ListTile(
-              title: Text(
-                '\nDonor type:  ' +
-                    donor[0].get("type").toString() +
-                    '\n\nName:  ' +
-                    donor[0].get("name").toString() +
-                    '\n\nContact number:  ' +
-                    donor[0].get("phone").toString() +
-                    '\n\nLocation:  ' +
-                    donor[0].get("location").toString(),
-                overflow: TextOverflow.visible,
-              ),
-            ),
-          ],
-        ),
-      ),
+          ])),
     ));
   }
 }
