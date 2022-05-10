235d1053bf3a96789a392357e5d8b208152b5789
njood97202 <njood97202@gmail.com>
2022-05-10 04:28:40 +0300

    release2

--- hayat_gp2_18-master/ios/Flutter/flutter_export_environment.sh
+++ hayat_gp2_18-master/ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ghadah/Developer/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ghadah/Documents/Hayat/hayat_gp2_18-master"
+export "FLUTTER_APPLICATION_PATH=/Users/ghadah/Desktop/gitHub/Hayat/hayat_gp2_18-master"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=/Users/ghadah/Documents/Hayat/hayat_gp2_18-master/lib/main.dart"
+export "FLUTTER_TARGET=/Users/ghadah/Desktop/gitHub/Hayat/hayat_gp2_18-master/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
@@ -11,4 +11,4 @@ export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
-export "PACKAGE_CONFIG=/Users/ghadah/Documents/Hayat/hayat_gp2_18-master/.dart_tool/package_config.json"
+export "PACKAGE_CONFIG=/Users/ghadah/Desktop/gitHub/Hayat/hayat_gp2_18-master/.dart_tool/package_config.json"
