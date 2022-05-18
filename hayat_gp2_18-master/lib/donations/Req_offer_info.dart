44a1fa4d6ac9f97ae7b2e3db261ddb1c3ef822a9
NjoodAlmousabhi <77965800+NjoodAlmousabhi@users.noreply.github.com>
2022-05-19 01:48:38 +0300

    Update Req_offer_info.dart

--- hayat_gp2_18-master/lib/donations/Req_offer_info.dart
+++ hayat_gp2_18-master/lib/donations/Req_offer_info.dart
@@ -111,146 +111,137 @@ class _ReqOfferInfo extends State<ReqOfferInfo> {
     var P = this.SelectedPic;
     var I = this.SelectedDonorId;
     var c;
-    return MaterialApp(
-        home: Scaffold(
+    return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.blueGrey[200],
+      appBar: AppBar(
+        title: Text(
+          'Hayat food donation',
+        ),
+        backgroundColor: Colors.teal[200],
+        elevation: 0.0,
+      ),
       body: Container(
-          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          padding: EdgeInsets.all(10),
-          alignment: Alignment.center,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.all(
-                Radius.circular(10.0) //                 <--- border radius here
-                ),
-            color: Colors.white70,
-          ),
-          child: ListView(scrollDirection: Axis.vertical, children: <Widget>[
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        padding: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.all(
+              Radius.circular(10.0) //                 <--- border radius here
+              ),
+          color: Colors.white70,
+        ),
+        child: ListView(
+          scrollDirection: Axis.vertical,
+          children: <Widget>[
+            Text(
+              '\nDonation offer details',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            Text('\n\nFood category of the donation:',
+                overflow: TextOverflow.visible,
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              '\n' + C + '\n',
+              overflow: TextOverflow.visible,
+            ),
+            Text('\nFood Status of Donation: \n',
+                overflow: TextOverflow.visible,
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              '' + S + '\n',
+              overflow: TextOverflow.visible,
+            ),
+            Text('\nAvailable quantity (# person): \n',
+                overflow: TextOverflow.visible,
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              '' + A + '\n',
+              overflow: TextOverflow.visible,
+            ),
+            Text('\nExpiration date: : \n',
+                overflow: TextOverflow.visible,
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            Text(
+              '' + E + '\n',
+              overflow: TextOverflow.visible,
+            ),
+            Divider(color: Colors.grey),
             Container(
-              child: Container(
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                      child: Text(
-                        '\nDonation offer details',
+                alignment: Alignment.center,
+                height: 280.0,
+                color: Colors.teal[200],
+                child: Image.network(
+                  P!.url!,
+                  fit: BoxFit.fitHeight,
+                )),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.blueGrey[200]),
+                  ),
+                  onPressed: () async {
+                    // set up the buttons
+                    Widget cancelButton = TextButton(
+                      child: Text("Ok"),
+                      onPressed: () {
+                        Navigator.pop(context, 'OK');
+                      },
+                    );
+                    Widget continueButton = TextButton(
+                      child: Text("Contact"),
+                      onPressed: () {
+                        //what's app
+                        // c = int.parse(donor[0].get("phone"));
+                        c = donor[0].get("phone").toString();
+                        print('hey ' + c);
+                        () async =>
+                            await launch("https://wa.me/${c}?text=Hello");
+                      },
+                    );
+
+                    // set up the AlertDialog
+                    AlertDialog alert = AlertDialog(
+                      title: Text(
+                        '\nDonor Information',
                         style: TextStyle(fontWeight: FontWeight.bold),
                         textAlign: TextAlign.center,
                       ),
-                    ),
-                    Container(
-                      child: ListTile(
-                        title: Text(
-                          '\nFood category of the donation: \n',
+                      content: Container(
+                        child: Text(
+                          '\nDonor type:  ' +
+                              donor[0].get("type").toString() +
+                              '\n\nName:  ' +
+                              donor[0].get("name").toString() +
+                              '\n\nContact number:  ' +
+                              donor[0].get("phone").toString() +
+                              '\n\nLocation:  ' +
+                              donor[0].get("location").toString(),
                           overflow: TextOverflow.visible,
                         ),
-                        subtitle: Text(C),
                       ),
-                    ),
-                    Container(
-                      child: ListTile(
-                        title: Text(
-                          '\nFood Status of Donation: \n',
-                          overflow: TextOverflow.visible,
-                        ),
-                        subtitle: Text(S),
-                      ),
-                    ),
-                    Container(
-                      child: ListTile(
-                        title: Text(
-                          '\nAvailable quantity (# person): \n',
-                          overflow: TextOverflow.visible,
-                        ),
-                        subtitle: Text(A),
-                      ),
-                    ),
-                    Container(
-                      child: ListTile(
-                        title: Text(
-                          '\nExpiration date: : \n',
-                          overflow: TextOverflow.visible,
-                        ),
-                        subtitle: Text(E),
-                      ),
-                    ),
-                    Divider(color: Colors.grey),
-                    Container(
-                        alignment: Alignment.center,
-                        height: 280.0,
-                        color: Colors.teal[200],
-                        child: Image.network(
-                          P!.url!,
-                          fit: BoxFit.fitHeight,
-                        )),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 8, vertical: 16),
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.blueGrey[200]),
-                          ),
-                          onPressed: () async {
-                            // set up the buttons
-                            Widget cancelButton = TextButton(
-                              child: Text("Ok"),
-                              onPressed: () {
-                                Navigator.pop(context, 'OK');
-                              },
-                            );
-                            Widget continueButton = TextButton(
-                              child: Text("Contact"),
-                              onPressed: () {
-                                //what's app
-                                // c = int.parse(donor[0].get("phone"));
-                                c = donor[0].get("phone").toString();
-                                print('hey ' + c);
-                                () async => await launch(
-                                    "https://wa.me/${c}?text=Hello");
-                              },
-                            );
+                      actions: [
+                        cancelButton,
+                        continueButton,
+                      ],
+                    );
 
-                            // set up the AlertDialog
-                            AlertDialog alert = AlertDialog(
-                              title: Text(
-                                '\nDonor Information',
-                                style: TextStyle(fontWeight: FontWeight.bold),
-                                textAlign: TextAlign.center,
-                              ),
-                              content: Container(
-                                child: Text(
-                                  '\nDonor type:  ' +
-                                      donor[0].get("type").toString() +
-                                      '\n\nName:  ' +
-                                      donor[0].get("name").toString() +
-                                      '\n\nContact number:  ' +
-                                      donor[0].get("phone").toString() +
-                                      '\n\nLocation:  ' +
-                                      donor[0].get("location").toString(),
-                                  overflow: TextOverflow.visible,
-                                ),
-                              ),
-                              actions: [
-                                cancelButton,
-                                continueButton,
-                              ],
-                            );
-
-                            // show the dialog
-                            showDialog(
-                              context: context,
-                              builder: (BuildContext context) {
-                                return alert;
-                              },
-                            );
-                          },
-                          child: const Text('Donor Information')),
-                    ),
-                  ],
-                ),
-              ),
+                    // show the dialog
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return alert;
+                      },
+                    );
+                  },
+                  child: const Text('Donor Information')),
             ),
-          ])),
-    ));
+          ],
+        ),
+      ),
+    );
   }
 }
