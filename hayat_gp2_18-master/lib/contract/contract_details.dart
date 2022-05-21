76324a80e76b3a509236747fa915cfb89540ae61
njood97202 <njood97202@gmail.com>
2022-05-21 16:08:17 +0300

    release 2

--- hayat_gp2_18-master/lib/contract/contract_details.dart
+++ hayat_gp2_18-master/lib/contract/contract_details.dart
@@ -1,9 +1,14 @@
+import 'dart:math';
+import 'dart:io' as Platform;
+
 import 'package:flutter/material.dart';
 import 'package:hayat_gp2_18/home_pages/donor_home.dart';
 import 'package:hayat_gp2_18/donations/search_offers.dart';
 import 'package:parse_server_sdk_flutter/parse_server_sdk.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class contractDetailesForDonor extends StatelessWidget {
+  List<ParseObject> ChO = <ParseObject>[];
   var SelectedcontCategory;
   var SelectedcontStatus;
   var SelectedAvailableQuantity_c;
@@ -41,7 +46,7 @@ class contractDetailesForDonor extends StatelessWidget {
     List<ParseObject> ChO = <ParseObject>[];
 
 // function to retrive all contracts with same Charity ID from database
-    void getContracts(String CID) async {
+    /*void getContracts(String CID) async {
       QueryBuilder<ParseUser> queryUsers =
           QueryBuilder<ParseUser>(ParseUser.forQuery())
             ..whereEqualTo("cho_id", CID);
@@ -55,6 +60,40 @@ class contractDetailesForDonor extends StatelessWidget {
         print('cho' + ChO[0].get(("name")));
       } else {
         ChO = [];
+      }
+    }*/
+
+    void getDonor(String SelectedchoId) async {
+      QueryBuilder<ParseUser> queryUsers =
+          QueryBuilder<ParseUser>(ParseUser.forQuery())
+            ..whereEqualTo("objectId", SelectedchoId);
+
+      final ParseResponse apiResponse = await queryUsers.query();
+
+      if (apiResponse.success) {
+        ChO = apiResponse.results as List<ParseObject>;
+        print('donors');
+        print(ChO);
+        print(ChO[0].get('name'));
+        print(ChO[0].get('phone'));
+      } else {
+        ChO = [];
+      }
+    }
+
+    openwhatsapp(phone) async {
+      print('access openwhatsapp ');
+      print(phone);
+      var whatsappURl_android =
+          "whatsapp://send?phone=" + phone + "&text=hello";
+      var whatappURL_ios = "https://wa.me/$phone?text=${Uri.parse("hello")}";
+
+      // android
+      if (await canLaunch(whatsappURl_android)) {
+        await launch(whatsappURl_android);
+      } else {
+        ScaffoldMessenger.of(context)
+            .showSnackBar(SnackBar(content: new Text("whatsapp no installed")));
       }
     }
 
@@ -121,6 +160,69 @@ class contractDetailesForDonor extends StatelessWidget {
               overflow: TextOverflow.visible,
             ),
             Divider(color: Colors.grey),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
+              child: ElevatedButton(
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.blueGrey[200]),
+                  ),
+                  onPressed: () async {
+                    // set up the buttons
+                    print(CID);
+                    getDonor(CID);
+                    Widget cancelButton = TextButton(
+                      child: Text("Ok"),
+                      onPressed: () {
+                        Navigator.pop(context, 'OK');
+                      },
+                    );
+                    Widget continueButton = TextButton(
+                      child: Text("Contact"),
+                      onPressed: () {
+                        //what's app
+
+                        var Userphone = ChO[0].get('phone').toString();
+                        print('hey ' + ChO[0].get('phone').toString());
+                        print(Userphone);
+                        openwhatsapp(Userphone);
+                      },
+                    );
+
+                    // set up the AlertDialog
+                    AlertDialog alert = AlertDialog(
+                      title: Text(
+                        '\ncharity Information',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                      content: Container(
+                        child: Text(
+                          '\nName:  ' +
+                              ChO[0].get("name").toString() +
+                              '\n\nContact number:  ' +
+                              ChO[0].get("phone").toString() +
+                              '\n\nLocation:  ' +
+                              ChO[0].get("location").toString(),
+                          overflow: TextOverflow.visible,
+                        ),
+                      ),
+                      actions: [
+                        cancelButton,
+                        continueButton,
+                      ],
+                    );
+
+                    // show the dialog
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return alert;
+                      },
+                    );
+                  },
+                  child: const Text('charity Information')),
+            ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 16),
               child: ElevatedButton(
